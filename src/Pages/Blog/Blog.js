4b1d518ec67f583a35806d3205f30b12c8b29ada
asfiqurrahmanomey <omeyjkt@gmail.com>
2022-11-25 20:58:53 +0600

    footer

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -5,30 +5,33 @@ const Blog = () => {
         <div>
             <section class="py-10 bg-white sm:py-16 lg:py-24">
                 <div class="max-w-6xl px-4 mx-auto sm:px-6 lg:px-8">
-                    <h2 class="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl text-center">Our Blog Section</h2>
+                    <h2 class="text-3xl font-bold leading-tight text-gray-700 sm:text-4xl lg:text-5xl text-center" data-aos="fade-up"
+                        data-aos-anchor-placement="center-bottom" data-aos-duration="3000">Our Blog Section</h2>
 
                     <div class="flow-root mt-12 sm:mt-16">
-                        <div class="divide-y divide-gray--200 -my-9">
-                            <div class="py-9">
-                                <p class="text-xl font-semibold text-black">How to create an account?</p>
-                                <p class="mt-3 text-base text-gray-600">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
-                                <p class="mt-3 text-base text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <div class="divide-y divide-gray--200 -my-9" data-aos="zoom-in" data-aos-duration="3000">
+                            <div class="py-9 text-justify ">
+                                <p class="text-xl font-semibold text-black">What are the different ways to manage a state in a React application?</p>
+                                <p class="mt-3 text-base text-gray-600">There are four main types of state you need to properly manage in your React apps:
+                                    <span className='text-orange-600'> 1. Local State</span> <span className='text-orange-600'> 2. Global State</span> <span className='text-orange-600'> 3. Server State</span> <span className='text-orange-600'> 4. URL State .</span>Here we provide a variable name for the state (count) and a function name we'll use every time we need to update that state (setCount). Last, we set the initial value of the state (0), which will be the value loaded by default every time the app starts. <br />Lastly, as mentioned above, every time we want to update the state we have to use the function we declared: setCount To use it, we just need to call it passing it the new state we want as a parameter. That is, if we want to add 1 to the previous estate, we call setCount(count+1).</p>
                             </div>
 
-                            <div class="py-9">
-                                <p class="text-xl font-semibold text-black">What payment method do you support?</p>
-                                <p class="mt-3 text-base text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam.</p>
+                            <div class="py-9 text-justify">
+                                <p class="text-xl font-semibold text-black">How does prototypical inheritance work?</p>
+                                <p class="mt-3 text-base text-gray-600">The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the [[Prototype]] of an object, we use Object. getPrototypeOf and Object.</p>
                             </div>
 
-                            <div class="py-9">
-                                <p class="text-xl font-semibold text-black">What payment method do you support?</p>
-                                <p class="mt-3 text-base text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                            <div class="py-9 text-justify">
+                                <p class="text-xl font-semibold text-black">What is a unit test? Why should we write unit tests?</p>
+                                <p class="mt-3 text-base text-gray-600">The main objective of unit testing is to isolate written code to test and determine if it works as intended. Unit testing is an important step in the development process, because if done correctly, it can help detect early flaws in code which may be more difficult to find in later testing stages.</p>
                             </div>
 
-                            <div class="py-9">
-                                <p class="text-xl font-semibold text-black">How do you provide support?</p>
+                            <div class="py-9 text-justify">
+                                <p class="text-xl font-semibold text-black">React vs. Angular vs. Vue?</p>
                                 <p class="mt-3 text-base text-gray-600">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt <a href="#" title="" class="text-blue-600 transition-all duration-200 hover:text-blue-700 focus:text-blue-700 hover:underline">support@Celebration.com</a>
+                                    <span className='text-orange-600'>Angular</span> is a platform and framework for building single-page client applications using HTML and TypeScript. Angular is written in TypeScript. It implements core and optional functionality as a set of TypeScript libraries that you import into your applications.<br />
+                                    <span className='text-orange-600'>Vue. js (or simply Vue)</span> is a lightweight, JavaScript framework for building reactive web user interfaces. Vue extends standard HTML and CSS to create a suite of powerful tools for building the front end of interactive web applications. <br />
+                                    <span className='text-orange-600'> REACT</span> It's used for building interactive user interfaces and web applications quickly and efficiently with significantly less code than you would with vanilla JavaScript. In React, you develop your applications by creating reusable components that you can think of as independent Lego blocks.
                                 </p>
                             </div>
                         </div>
