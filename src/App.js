dc0f2b633656187c7c437324529fe28e8ad449f5
asfiqurrahmanomey <omeyjkt@gmail.com>
2022-11-25 21:30:23 +0600

    blog added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <RouterProvider router={router}>
 
       </RouterProvider>
+
       <Toaster />
     </div>
   );
--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Pages/Shared/Footer/Footer';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
+import ScrollToTop from "react-scroll-to-top";
 
 const Main = () => {
+
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <ScrollToTop smooth />
             <Footer></Footer>
         </div>
     );
